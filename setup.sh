e332bca876b0731d22c274f0f86228061ea7aec8
thr3a <thr3a@example.com>
2022-12-10 11:42:06 +0900

    fix: ビルドできないのを修正

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cp -r /tmp/mantine/src/mantine-demos src
 find -name '*.story.tsx' -delete
 cp -f /tmp/mantine/configuration/types/*.d.ts ./src
 cp -f /tmp/mantine/src/mantine-styles/src/emotion.d.ts ./src
+rm -rf ./src/mantine-demos/src/demos/tiptap/
 
 sed -i '/AffixDemos/s/^/\/\//g' src/mantine-demos/src/index.ts
 sed -i '/TypographyStylesProviderDemos/s/^/\/\//g' src/mantine-demos/src/index.ts
@@ -13,3 +14,4 @@ sed -i '/Portal/s/^/\/\//g' src/mantine-demos/src/index.ts
 sed -i '/ModalsDemos/s/^/\/\//g' src/mantine-demos/src/index.ts
 sed -i '/NotificationsDemos/s/^/\/\//g' src/mantine-demos/src/index.ts
 sed -i '/PrismDemos/s/^/\/\//g' src/mantine-demos/src/index.ts
+sed -i '/TipTapDemos/s/^/\/\//g' src/mantine-demos/src/index.ts
